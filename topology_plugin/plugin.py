8c0e4ad26c50711724da9aa5b92fa13996790ed3
Michael Sieger <sieger.michael@swm.de>
2017-05-09 16:03:00 +0200

    Add icons for the topology plugin. Partial implementation of outage and finding of providers.

--- topology_plugin/plugin.py
+++ topology_plugin/plugin.py
@@ -8,6 +8,7 @@ from ui.overlay import *
 
 import logging
 
+
 class Plugin:
 
     def __init__(self, iface):
@@ -21,11 +22,28 @@ class Plugin:
     def on_request_fail(self, text):
         self.iface.messageBar().pushMessage("Error", text)
 
+    def check_selected(self, start, end):
+        start_missing = (start and self.selection.get_selected_start() is None)
+        end_missing = (end and self.selection.get_selected_end() is None)
+        text = None
+        if start_missing and end_missing:
+            text = u"Start und Ende müssen für diese Operation ausgewählt werden"
+        elif start_missing:
+            text = u"Ein Start muss für diese Operation ausgewählt werden"
+        elif end_missing:
+            text = u"Ein Ende muss für diese Operation ausgewählt werden"
+        if text is not None:
+            self.iface.messageBar().pushMessage("Info", text)
+            return False
+        return True
+
     def _node_from_selection(self, sel, callback):
         self.rest.get_json('/node', sel.schema, {'rwo_name': sel.rwo_name, 'geom_name': sel.geom_name, 'x': sel.x, 'y':sel.y},
                            callback)
 
     def shortest_path(self):
+        if not self.check_selected(True, True):
+            return
         start = self.selection.get_selected_start()
         end = self.selection.get_selected_end()
 
@@ -46,7 +64,8 @@ class Plugin:
         self._node_from_selection(start, start_node_callback)
 
     def blocking_nodes(self):
-
+        if not self.check_selected(True, False):
+            return
         sel = self.selection.get_selected_start()
 
         def blocked_path_callback(blocked_path):
@@ -54,6 +73,32 @@ class Plugin:
 
         def node_callback(node):
             self.rest.get_json('/blocked_path', sel.schema, {'node_id': node["id"]}, blocked_path_callback)
+
+        self._node_from_selection(sel, node_callback)
+
+    def providers(self):
+        if not self.check_selected(True, False):
+            return
+        sel = self.selection.get_selected_start()
+
+        def providers_callback(providers):
+            print(str(providers))
+
+        def node_callback(node):
+            self.rest.get_json('/node', sel.schema, {'provides': node["id"]}, providers_callback)
+
+        self._node_from_selection(sel, node_callback)
+
+    def outage_analysis(self):
+        if not self.check_selected(True, False):
+            return
+        sel = self.selection.get_selected_start()
+
+        def unreachable_callback(resp):
+            print(str(resp))
+
+        def node_callback(node):
+            self.rest.get_json('/node', sel.schema, {'unreachable-when-blocked': node["id"]}, unreachable_callback)
 
         self._node_from_selection(sel, node_callback)
 
@@ -67,10 +112,12 @@ class Plugin:
     def initGui(self):
         self.selection = Selection(self.iface)
         self.overlay = Overlay(self.iface)
-        self.toolbar.add("Blocking Nodes", "circle-filled.png", self.blocking_nodes)
-        self.toolbar.add("Shortest Path", "circle-filled.png", self.shortest_path)
-        self.toolbar.add("Select start", "Cross1.png", self.select_start)
-        self.toolbar.add("Select end", "Cross1.png", self.select_end)
+        self.toolbar.add(u"Sperrstrecke", "blocked_path.png", self.blocking_nodes)
+        self.toolbar.add(u"Kürzester Weg", "path.png", self.shortest_path)
+        self.toolbar.add(u"Versorger finden", "provider.png", self.providers)
+        self.toolbar.add(u"Betroffene Verbraucher finden", "consumer.png", self.outage_analysis)
+        self.toolbar.add(u"Start", "Cross1.png", self.select_start)
+        self.toolbar.add(u"Ende", "Cross1.png", self.select_end)
 
     def unload(self):
         self.toolbar.remove_all()
